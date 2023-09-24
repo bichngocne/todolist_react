fd42d1499f6e17352ce0cd17f33579c3ff9a3f49
ngocty <nb447105@gmail.com>
2023-09-24 16:44:39 +0700

    useState get value input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 function App() {
+  const [work, setWork] = useState('');
+  console.log(work)
+
+
   return (
-    <div className="App">
-     Welcome come to reactjs
+    <div className="flex justify-center items-center py-3">
+    <input type="text"
+    className="outline-none border border-red-300 px-4 py-2 w-[500]"
+    placeholder="Nhập dữ liệu ...."
+    value={work}
+    onChange={e => setWork(e.target.value)}
+    />
+    <button className="outline-none py-2 px-4 text-white bg-red-400">Add</button>
     </div>
   );
 }
